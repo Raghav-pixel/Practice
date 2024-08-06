90d35b158b090d5c08da58f881442f9ebe1418b9
Raghav Khanna <raghavkhanna@Raghavs-MacBook-Pro.local>
2024-08-06 20:35:54 +0530

    added polyfill for call and apply

--- Js-practice/Polyfills/this/call-apply-bind.js
+++ Js-practice/Polyfills/this/call-apply-bind.js
@@ -4,10 +4,34 @@ Function.prototype.myBind = function(obj={}, ...args) {
     const fn = this;
 
     if (typeof fn !== "function") {
-        throw new Error("Invalid function provided for binding");
+        throw new Error("cannot be bound as it is not callable");
     }
     return function(...args2) {
         fn.apply(obj, [...args, ...args2]);
     }
 }
 
+// polyfill for call
+
+Function.prototype.myCall = function(context={}, ...args) {
+    if(typeof this !== "function") {
+        throw new Error("Invalid function provided for call");
+    }
+
+    context.fn = this;
+    context.fn(...args);
+}
+
+// polyfill for apply
+
+Function.prototype.myApply = function(context={}, args=[]) {
+    if(typeof this !== "function") {
+        throw new Error("Inavlid function provided for apply");
+    }
+    if(!Array.isArray(args)) {
+        throw new Error("CreateListFromArrayLike is called on non-object");
+    }
+
+    context.fn = this;
+    context.fn(...args);
+}
